bb3b4424b7cebe97f115c3e45b71f692caf085aa
DaviVGuilherme <168041183+DaviVGuilherme@users.noreply.github.com>
2024-06-18 21:24:31 -0300

    romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -1,28 +1,23 @@
-from romans import int_to_roman, roman_to_int
-def test_int_to_roman():
-    assert int_to_roman(1) == "sym"
-
-def test_roman_to_int():
-    assert roman_to_int("I") == num
-    
-def printRomano(number):
+def int_to_roman(number):
     num = [1, 4, 5, 9, 10, 40, 50, 90,
-        100, 400, 500, 900, 1000]
+           100, 400, 500, 900, 1000]
     sym = ["I", "IV", "V", "IX", "X", "XL",
-        "L", "XC", "C", "CD", "D", "CM", "M"]
+           "L", "XC", "C", "CD", "D", "CM", "M"]
     i = 12
-      
+    roman_numeral = ""
+    
     while number:
         div = number // num[i]
         number %= num[i]
   
         while div:
-            print(sym[i], end = "")
+            roman_numeral += sym[i]
             div -= 1
         i -= 1
+    
+    return roman_numeral
   
 if __name__ == "__main__":
     number = 1954
-    print("O número convertido pra romano é: ", end = "")
-    printRomano(number)
-    
+    print("O número convertido para romano é: ", end="")
+    print(int_to_roman(number))
