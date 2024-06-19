5ca90ea86e3c9197334e4cbbaf4209a75a9946c9
DaviVGuilherme <168041183+DaviVGuilherme@users.noreply.github.com>
2024-06-18 22:00:18 -0300

    romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -1,23 +1,39 @@
+num = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+
+sym = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
 def int_to_roman(number):
-    num = [1, 4, 5, 9, 10, 40, 50, 90,
-           100, 400, 500, 900, 1000]
-    sym = ["I", "IV", "V", "IX", "X", "XL",
-           "L", "XC", "C", "CD", "D", "CM", "M"]
-    i = 12
+    i = 0
     roman_numeral = ""
-    
-    while number:
+    while number > 0:
         div = number // num[i]
         number %= num[i]
-  
-        while div:
+        while div > 0:
             roman_numeral += sym[i]
             div -= 1
-        i -= 1
-    
+        i += 1
     return roman_numeral
-  
-if __name__ == "__main__":
-    number = 1954
-    print("O número convertido para romano é: ", end="")
-    print(int_to_roman(number))
+sym_to_value = {
+    "M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100,
+    "XC": 90, "L": 50, "XL": 40, "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1
+}
+
+def roman_to_int(s):
+    i = 0
+    total = 0
+    n = len(s)
+    while i < n:
+        if i + 1 < n and s[i:i+2] in sym_to_value:
+            total += sym_to_value[s[i:i+2]]
+            i += 2
+        else:
+            total += sym_to_value[s[i]]
+            i += 1
+    return total
+
+if _name_ == "_main_":
+    number = 1994
+    s = "MCMXCIV"
+
+    print("O número romano para o numeral inserido é:", int_to_roman(number))
+    print("O número inteiro para o numeral romano inserido é:", roman_to_int(s))
